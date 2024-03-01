c93cbc0419a26c256f10a0a27c745d40179c9956
Petrit Berisha <petrit.berisha@led-con.com>
2024-03-01 09:59:06 +0100

    Start Process

--- start.py
+++ start.py
@@ -2,7 +2,8 @@
 import subprocess
 import uuid
 
+
 genpool = 2000
 filename = "../btcgen_data/" + str(uuid.uuid4()) + ".csv"
 print(f"Starting Generation process -- Generation {genpool} saving to {filename}")
-subprocess.call(["python3", "genbtcadd.py", genpool, filename])
+subprocess.call(["python3", "genbtcadd.py", str(genpool), str(filename)])
