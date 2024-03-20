6460fab9747fbea0a20c09607cde356bacfeb5f6
Petrit Berisha <petrit.berisha@led-con.com>
2024-03-20 22:14:12 +0100

    asdf

--- combined.py
+++ combined.py
@@ -9,7 +9,7 @@ from datetime import datetime
 from pprint import pprint
 
 startTime = datetime.now()
-num_addresses = sys.argv[1]
+num_addresses = 100 #sys.argv[1]
 # Set the number of addresses to generate
 
 data = []
@@ -25,8 +25,32 @@ for i in range(int(num_addresses)):
     new_row = {addr2, addr1, addr3, addr4, priv}
     data.append(new_row)
 
-df = pd.DataFrame(data)
-pprint(vars(data))
-#pprint(vars(df))
-print("Generated " + num_addresses + " and it took " + str(datetime.now() - startTime))
+df_generated = pd.DataFrame(data)
 
+#start duplicate check against real addresses with gpu acceleration
+
+real_csv_filename = '../btcgen_data/richaddresses.csv'
+output_csv_path = csv_dir + "result.csv"
+
+#read richaddresses
+df_real = pd.read_csv(real_csv_filename)
+
+r_column_to_compare = df_real.columns[0]
+g_column_to_compare0 = df_generated.columns[0]
+g_column_to_compare1 = df_generated.columns[1]
+g_column_to_compare2 = df_generated.columns[2]
+g_column_to_compare3 = df_generated.columns[3]
+
+matches0 = df_generated[g_column_to_compare0].isin(df_real[r_column_to_compare])
+df_matched0 = df_generated[matches0]
+matches1 = df_generated[g_column_to_compare1].isin(df_real[r_column_to_compare])
+df_matched1 = df_generated[matches1]
+matches2 = df_generated[g_column_to_compare2].isin(df_real[r_column_to_compare])
+df_matched2 = df_generated[matches2]
+matches3 = df_generated[g_column_to_compare3].isin(df_real[r_column_to_compare])
+df_matched3 = df_generated[matches3]
+
+df_matched0.to_csv(output_csv_path, mode='a', index=False, header=False)
+df_matched1.to_csv(output_csv_path, mode='a', index=False, header=False)
+df_matched2.to_csv(output_csv_path, mode='a', index=False, header=False)
+df_matched3.to_csv(output_csv_path, mode='a', index=False, header=False)
